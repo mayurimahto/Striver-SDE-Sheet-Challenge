9c42dc2c939a8445d65523f5fd2484b28d390a80
Mayuri Mahto <89357682+mayurimahto@users.noreply.github.com>
2023-06-21 18:22:15 +0530

    Update Set Matrix Zeros.cpp

--- Set Matrix Zeros.cpp
+++ Set Matrix Zeros.cpp
@@ -38,3 +38,28 @@ void setZeros(vector<vector<int>> &matrix)
         }
     }
 }
+
+//BETTER
+void setZeros(vector<vector<int>> &matrix){
+    // Write your code here.
+	int n=matrix.size();
+	int m=matrix[0].size();
+	vector<int> row (n,0);
+	vector<int> cols (m,0);
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){			
+			if(matrix[i][j]==0){
+				row[i]=1;
+				cols[j]=1;
+			}
+		}
+	}
+	// converting to 0s
+    for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			if (row[i] == 1 || cols[j] == 1){
+			matrix[i][j] = 0;
+			}
+		}
+	}
+}
